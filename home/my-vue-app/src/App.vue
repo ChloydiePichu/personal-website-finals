e7ab200ec7ab9feaa85f21475224f0fda12331fa
Chloyd Lechico <137134954+ChloydiePichu@users.noreply.github.com>
2025-03-05 14:20:31 +0000

    Integrated vue

---
+++ home/my-vue-app/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+    <h1>My Personal Website</h1>
+    <SurveyForm />
+  </div>
+</template>
+
+<script>
+import SurveyForm from "./components/SurveyForm.vue";
+
+export default {
+  components: {
+    SurveyForm,
+  },
+};
+</script>
+
+<style>
+body {
+  font-family: Arial, sans-serif;
+  text-align: center;
+  background-color: #0d1117;
+  color: white;
+}
+</style>
