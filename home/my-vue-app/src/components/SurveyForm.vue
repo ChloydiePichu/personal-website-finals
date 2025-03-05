cada95a98335306eb6e86d97417ce1a82a271be3
Chloyd Lechico <137134954+ChloydiePichu@users.noreply.github.com>
2025-03-05 18:03:55 +0000

    Try update

--- home/my-vue-app/src/components/SurveyForm.vue
+++ home/my-vue-app/src/components/SurveyForm.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <div id="container">
-      <img id="profile-image" src="Lechico HDD.jpg" />
+      <img id="profile-image" src="/home/Lechico HDD.jpg" />
       <div id="about-me">
         <h2>About Me</h2>
         <p>Hello, my name is Chloyd! I am 20 years old and I am currently a 2nd year college student at APC.</p>
@@ -133,7 +133,7 @@ export default {
       thoughts: "",
       rating: 5,
       successMessage: "",
-      images: ["gala.jpg", "pc pic.jpg"],
+      images: ["/home/gala.jpg", "/home/pc pic.jpg"],
       currentImage: 0,
     };
   },
